59e71a2f5ac81f434dd3c0c69ca4b1222902fe3b
Fabian Melendez <fabmelb@gmail.com>
2015-03-18 08:41:07 -0600

    Pipeline fix
    Signed-off-by: Fabian Melendez <fabmelb@gmail.com>

--- experimento_1/inputs/Module_ROM.v
+++ experimento_1/inputs/Module_ROM.v
@@ -2,7 +2,7 @@
 `include "Defintions.v"
 
 `define LOOP1 8'd8
-`define LOOP2 8'd5
+`define LOOP2 8'd7
 module ROM
 (
 	input  wire[15:0]  		iAddress,
@@ -16,19 +16,19 @@ begin
 	1: oInstruction = { `STO , `R7,16'b0001 };
 	2: oInstruction = { `STO ,`R3,16'h1     }; 
 	3: oInstruction = { `STO, `R4,16'd1000 };
-	4: oInstruction = { `STO, `R5,16'd0     };  //j
+	4: oInstruction = { `STO, `R5,16'd0     }; 
+	5: oInstruction = { `STO ,`R1,16'h0     }; 	
+	6: oInstruction = { `STO ,`R2,16'd65000 }; //j
 //LOOP2:
-	5: oInstruction = { `LED ,8'b0,`R7,8'b0 };
-	6: oInstruction = { `STO ,`R1,16'h0     }; 	
-	7: oInstruction = { `STO ,`R2,16'd65000 };
-//LOOP1:	
-	8: oInstruction = { `ADD ,`R1,`R1,`R3    }; 
-	9: oInstruction = { `BLE ,`LOOP1,`R1,`R2 }; 
+	7: oInstruction = { `LED ,8'b0,`R1,8'b0 };	
 	
-	10: oInstruction = { `ADD ,`R5,`R5,`R3    };
-	11: oInstruction = { `BLE ,`LOOP2,`R5,`R4 };	
-	12: oInstruction = { `NOP ,24'd4000       }; 
-	13: oInstruction = { `ADD ,`R7,`R7,`R3    };
+//LOOP1:	
+	8: oInstruction = { `ADD ,`R1,`R1,`R3    };
+	9: oInstruction = { `NOP ,24'd4000    };
+	10: oInstruction = { `NOP ,24'd4000    };
+	11: oInstruction = { `NOP ,24'd4000    };
+	12: oInstruction = { `NOP ,24'd4000    };
+	13: oInstruction = { `BLE ,`LOOP2,`R1,`R2 }; 
 	14: oInstruction = { `JMP ,  8'd2,16'b0   };
 	default:
 		oInstruction = { `LED ,  24'b10101010 };		//NOP
